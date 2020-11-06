cfe912466a26d6b9f21dc54de07a063c662a3271
asael-am <asael.alonzo@gmail.com>
2020-11-06 13:41:41 +0200

    update documentation

--- R/report.R
+++ R/report.R
@@ -1,212 +1,214 @@
-#' Print a full report of the time series model in a  varstan object
+#' Print a full report of the time series model in a  varstan object.
 #'
 #' The function returns a report with the users defined model for the given time series data
-#' and all the current defined priors of the model
+#' and all the current defined priors of the model.
 #'
-#' @usage  report(obj,...)
+#' @usage  report(object,...)
 #'
 #' @aliases report report.varstan report.Sarima report.garch report.varma report.Bekk report.naive
 #'
-#' @param obj an object varstan object or one of the defined current defined reports in varstan package
+#' @param object an object varstan object or one of the defined current defined reports in varstan package
 #' @param ... additional values need in print methods
 #'
-#' @details if \code{obj} is a varstan object the function will print the information of the
-#' defined model inside of the object. If \code{obj} is one of the model classes (like Sarima or garch)
+#' @details if \code{object} is a varstan object the function will print the information of the
+#' defined model inside of the object. If \code{object} is one of the model classes (like Sarima or garch)
 #' then it will print the report information as well.
 #'
-#'
 #' @author  Asael Alonzo Matamoros
-#'
-#' @seealso \code{\link{report}} \code{\link{prior}}
 #'
 #' @export
 #'
 #' @return  a  string with the defined time series report
 #'
 #' @examples
-#'
+#' library(astsa)
 #' dat2 = garch(birth,order = c(1,1,0))
 #' report(dat2)
 #'
-#' dat = varma(Astrovan,p=1,q=1)
+#' dat = varma(Astrovan,order = c(1,1))
 #' report(dat)
 #'
 #'
-report <- function(obj,...) {
+report <- function(object,...) {
   UseMethod("report")
 }
 #'
 #' @method report varstan
 #' @export
 #'
-report.varstan = function(obj){
-  if(!is.varstan(obj))
+report.varstan = function(object,...){
+  if(!is.varstan(object))
     stop("The current object is not a varstan class")
 
-  if( is.Sarima(obj$model)) report.Sarima(obj$model)
-  if( is.naive(obj$model))  report.naive(obj$model)
-  if( is.garch(obj$model))  report.garch(obj$model)
-  if( is.SVM(obj$model))    report.SVM(obj$model)
-  if( is.varma(obj$model))  report.varma(obj$model)
-  if( is.Bekk(obj$model))   report.Bekk(obj$model)
+  if( is.Sarima(object$model)) report.Sarima(object$model)
+  if( is.naive(object$model))  report.naive(object$model)
+  if( is.garch(object$model))  report.garch(object$model)
+  if( is.SVM(object$model))    report.SVM(object$model)
+  if( is.varma(object$model))  report.varma(object$model)
+  if( is.Bekk(object$model))   report.Bekk(object$model)
 }
 #'
 #' @method report Sarima
 #' @export
 #'
-report.Sarima = function(obj,...){
-  if( !is.Sarima(obj))
+report.Sarima = function(object,...){
+  if( !is.Sarima(object))
     stop("The object is not a Sarima model \n")
 
-  model.Sarima(obj)
+  model.Sarima(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n")
-  if(obj$p  > 0 )get_prior(model = obj,par = "ar")
-  if(obj$q  > 0 )get_prior(model = obj,par = "ma")
-  if(obj$P != 0 || obj$Q != 0 || obj$D != 0){
+  if(object$p  > 0 )get_prior(model = object,par = "ar")
+  if(object$q  > 0 )get_prior(model = object,par = "ma")
+  if(object$P != 0 || object$Q != 0 || object$D != 0){
     cat("\n Seasonal Parameters: \n")
-    if(obj$P  > 0 )get_prior(model = obj,par = "sar")
-    if(obj$Q  > 0 )get_prior(model = obj,par = "sma")
+    if(object$P  > 0 )get_prior(model = object,par = "sar")
+    if(object$Q  > 0 )get_prior(model = object,par = "sma")
   }
-  if(obj$d1 > 0 ){
+  if(object$d1 > 0 ){
     cat("\n Regression Parameters: \n")
-    get_prior(model = obj,par = "breg")
+    get_prior(model = object,par = "breg")
   }
 }
 #'
 #' @method report naive
 #' @export
 #'
-report.naive = function(obj,...){
-  if( !is.naive(obj))
+report.naive = function(object,...){
+  if( !is.naive(object))
     stop("The object is not a naive model \n")
 
-  model.naive(obj)
+  model.naive(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n")
-  if(obj$D > 0 ){
-    cat("\n period:",obj$period,"\n")
+  if(object$D > 0 ){
+    cat("\n period:",object$period,"\n")
   }
 }
 #'
 #' @method report garch
 #' @export
 #'
-report.garch = function(obj,...){
-  if(!is.garch(obj))
+report.garch = function(object,...){
+  if(!is.garch(object))
     stop("The object is not a garch model \n")
 
-  model.garch(obj)
+  model.garch(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n")
-  if(obj$s  > 0 )get_prior(model = obj,par = "arch")
-  if(obj$k  > 0 )get_prior(model = obj,par = "garch")
-  if(obj$h  > 0 )get_prior(model = obj,par = "mgarch")
-  if(obj$p != 0 || obj$q != 0 ){
+  if(object$s  > 0 )get_prior(model = object,par = "arch")
+  if(object$k  > 0 )get_prior(model = object,par = "garch")
+  if(object$h  > 0 )get_prior(model = object,par = "mgarch")
+  if(object$p != 0 || object$q != 0 ){
     cat("\n mean Parameters: \n")
-    if(obj$p  > 0 )get_prior(model = obj,par = "ar")
-    if(obj$q  > 0 )get_prior(model = obj,par = "ma")
+    if(object$p  > 0 )get_prior(model = object,par = "ar")
+    if(object$q  > 0 )get_prior(model = object,par = "ma")
   }
-  if(obj$d1 > 0 ){
+  if(object$d1 > 0 ){
     cat("\n Regression Parameters: \n")
-    get_prior(model = obj,par = "breg")
+    get_prior(model = object,par = "breg")
   }
-  if(obj$genT == TRUE){
+  if(object$genT){
     cat("\n Generalized t-student \n")
     cat("\n lambda ~ G(v/2,v/2) \n")
-    get_prior(obj,par = "dfv")
+    get_prior(model = object,par = "dfv")
+  }
+  if(object$asym1){
+    if(identical(object$asym,1)) cat("\n logistic asymmetric \n")
+    if(identical(object$asym,1)) cat("\n exponential asymmetric \n")
+    get_prior(model = object,par = "gamma")
   }
 }
 #'
 #' @method report SVM
 #' @export
 #'
-report.SVM = function(obj,...){
-  if(!is.SVM(obj))
+report.SVM = function(object,...){
+  if(!is.SVM(object))
     stop("The object is not a stochastic volatility model \n")
 
-  model.SVM(obj)
+  model.SVM(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n log Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n")
-  get_prior(model = obj,par = "alpha")
-  get_prior(model = obj,par = "beta")
-  if(obj$p != 0 || obj$q != 0 ){
+  get_prior(model = object,par = "alpha")
+  get_prior(model = object,par = "beta")
+  if(object$p != 0 || object$q != 0 ){
     cat("\n mean Parameters: \n")
-    if(obj$p  > 0 )get_prior(model = obj,par = "ar")
-    if(obj$q  > 0 )get_prior(model = obj,par = "ma")
+    if(object$p  > 0 )get_prior(model = object,par = "ar")
+    if(object$q  > 0 )get_prior(model = object,par = "ma")
   }
-  if(obj$d1 > 0 ){
+  if(object$d1 > 0 ){
     cat("\n Regression Parameters: \n")
-    get_prior(model = obj,par = "breg")
+    get_prior(model = object,par = "breg")
   }
 }
 #'
 #' @method report varma
 #' @export
 #'
-report.varma = function(obj,...){
-  if(!is.varma(obj))
+report.varma = function(object,...){
+  if(!is.varma(object))
     stop("The object is not a varma model \n")
 
-  model.varma(obj)
+  model.varma(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n mean Parameters: \n")
-  if(obj$p  > 0 )get_prior(model = obj,par = "ar")
-  if(obj$q  > 0 )get_prior(model = obj,par = "ma")
+  if(object$p  > 0 )get_prior(model = object,par = "ar")
+  if(object$q  > 0 )get_prior(model = object,par = "ma")
 
-  if(obj$s != 0 || obj$k != 0 || obj$h != 0 ){
+  if(object$s != 0 || object$k != 0 || object$h != 0 ){
     cat("\n Bekk Parameters: \n")
-    if(obj$s  > 0 )get_prior(model = obj,par = "arch")
-    if(obj$k  > 0 )get_prior(model = obj,par = "garch")
-    if(obj$h  > 0 )get_prior(model = obj,par = "mgarch")
+    if(object$s  > 0 )get_prior(model = object,par = "arch")
+    if(object$k  > 0 )get_prior(model = object,par = "garch")
+    if(object$h  > 0 )get_prior(model = object,par = "mgarch")
   }
-  if(obj$genT == TRUE){
+  if(object$genT){
     cat("\n Generalized t-student \n")
     cat("\n lambda ~ inverse.gamma(v/2,v/2) \n")
-    get_prior(obj,par = "dfv")
+    get_prior(object,par = "dfv")
   }
 }
 #'
 #' @method report Bekk
 #' @export
 #'
-report.Bekk = function(obj,...){
-  if(!is.Bekk(obj))
+report.Bekk = function(object,...){
+  if(!is.Bekk(object))
     stop("The object is not a Bekk model \n")
 
-  model.Bekk(obj)
+  model.Bekk(object)
   cat("Priors: \n Intercept:\n")
-  get_prior(obj,par = "mu0")
+  get_prior(object,par = "mu0")
   cat("\n Scale Parameter: \n")
-  get_prior(obj,par = "sigma0")
+  get_prior(object,par = "sigma0")
   cat("\n mean Parameters: \n")
-  if(obj$p  > 0 )get_prior(model = obj,par = "ar")
-  if(obj$q  > 0 )get_prior(model = obj,par = "ma")
+  if(object$p  > 0 )get_prior(model = object,par = "ar")
+  if(object$q  > 0 )get_prior(model = object,par = "ma")
 
-  if(obj$s != 0 || obj$k != 0 || obj$h != 0 ){
+  if(object$s != 0 || object$k != 0 || object$h != 0 ){
     cat("\n Bekk Parameters: \n")
-    if(obj$s  > 0 )get_prior(model = obj,par = "arch")
-    if(obj$k  > 0 )get_prior(model = obj,par = "garch")
-    if(obj$h  > 0 )get_prior(model = obj,par = "mgarch")
+    if(object$s  > 0 )get_prior(model = object,par = "arch")
+    if(object$k  > 0 )get_prior(model = object,par = "garch")
+    if(object$h  > 0 )get_prior(model = object,par = "mgarch")
   }
-  if(obj$genT == TRUE){
+  if(object$genT){
     cat("\n Generalized t-student \n")
     cat("\n lambda ~ inverse.gamma(v/2,v/2) \n")
-    get_prior(obj,par = "dfv")
+    get_prior(object,par = "dfv")
   }
 }
