8f790295ff9c290420929c5d4c0a42e69fd82a9d
asael-am <asael.alonzo@gmail.com>
2020-11-06 13:34:44 +0200

    documentation update

--- R/Sarima.R
+++ R/Sarima.R
@@ -1,28 +1,29 @@
-#' Constructor Seasonal arima model
+#' Constructor a Multiplicative Seasonal ARIMA model.
 #'
-#' Constructor of the Sarima model  for Bayesian estimation in STAN
+#' Constructor of the SARIMA model  for Bayesian estimation in \pkg{Stan}.
 #'
-#' The function returns  a list with  all the neccesary data for running a
-#' Seasonal arima model in stan
+#' The function returns a list with the data for running \code{stan()} function of
+#'  \pkg{rstan} package
 #'
-#' @usage Sarima(ts,order = c(1,0,0),seasonal = c(0,0),xreg = NULL,period = 0)
+#' @usage Sarima(ts,order = c(1,0,0),seasonal = c(0,0,0),xreg = NULL,period = 0,series.name = NULL)
 #'
-#' @param ts a univariate time series
+#' @param ts a numeric or ts object with the univariate time series.
 #' @param order A specification of the non-seasonal part of the ARIMA model: the
 #' three components (p, d, q) are the AR order, the number of differences, and the
 #' MA order.
 #' @param seasonal A specification of the seasonal part of the ARIMA model,same as
 #' order parameter:  the three components (p, d, q) are the seasonal AR order,
-#' the degree of seasonal differencing, and the seasonal MA order.
+#' the degree of seasonal differences, and the seasonal MA order.
 #' @param xreg	Optionally, a numerical matrix of external regressors,
 #' which must have the same number of rows as ts. It should not be a data frame.
 #' @param period an integer specifying the periodicity of the time series by
 #' default the value frequency(ts) is used.
 #' @param series.name an optional string vector with the series names.
 #'
-#' @details If \code{xreg} option is used, the model by default will cancel the
+#' @details
+#' If \code{xreg} option is used, the model by default will cancel the
 #' seasonal differences adjusted (D = 0). If a value \code{d} > 0 is used, all
-#' the regressor variables in xreg will be differenced aswell
+#' the regressor variables in \code{xreg} will be difference as well.
 #'
 #' The default priors used in Sarima are:
 #'
@@ -40,20 +41,35 @@
 #'
 #' @author  Asael Alonzo Matamoros
 #'
+#' @importFrom stats as.ts time frequency
+#' @importFrom utils tail
 #' @export
 #'
 #' @references
-#'  Box, George; Jenkins, Gwilym (1970).
-#'  Time Series Analysis: Forecasting and Control. San Francisco: Holden-Day.
+#' Box, G. E. P. and Jenkins, G.M. (1978). Time series analysis: Forecasting and
+#' control. San Francisco: Holden-Day. \emph{Biometrika}, 60(2), 297-303.
+#' \code{doi:10.1093/biomet/65.2.297}.
+#'
+#' Kennedy, P. (1992). Forecasting with dynamic regression models: Alan Pankratz, 1991.
+#' \emph{International Journal of Forecasting}. 8(4), 647-648.
+#' \code{url: https://EconPapers.repec.org/RePEc:eee:intfor:v:8:y:1992:i:4:p:647-648}.
+#'
+#' Hyndman, R. & Khandakar, Y. (2008). Automatic time series forecasting: the
+#' forecast package for \code{R}. \emph{Journal of Statistical Software}. 26(3),
+#' 1-22.\code{doi:	10.18637/jss.v027.i03}
 #'
 #' @seealso \code{\link{garch}} \code{\link{set_prior}}
 #'
 #' @examples
-#' # Declare a seasonal arima model for the birth data of asta package
+#' # Declare a multiplicative seasonal ARIMA model for the birth data.
 #'
+#' library(astsa)
 #' model = Sarima(birth,order = c(0,1,2),seasonal = c(1,1,1))
 #' model
 #'
+#' #Declare an Dynamic Harmonic Regression model for the birth data.
+#' model = Sarima(birth,order = c(1,0,1),xreg = fourier(birth,K = 2))
+#' model
 #'
 Sarima = function(ts,order = c(1,0,0),seasonal = c(0,0,0),xreg = NULL,
                   period = 0,series.name = NULL){
@@ -66,11 +82,11 @@ Sarima = function(ts,order = c(1,0,0),seasonal = c(0,0,0),xreg = NULL,
   else
     sn = as.character(series.name)
 
-  m1 = list(n = n,dimension = 1,time = as.numeric(time(ts)),
+  m1 = list(n = n,dimension = 1,time = as.numeric(stats::time(ts)),
             p = no_negative_check(order[1]),
             d = no_negative_check(order[2]),
             q = no_negative_check(order[3]),
-            yreal = as.ts(ts),y = as.numeric(ts),series.name = sn)
+            yreal = stats::as.ts(ts),y = as.numeric(ts),series.name = sn)
 
   m1$prior_mu0 = c(0,2.5,6,4)
   m1$prior_sigma0 = c(0,1,7,4)
@@ -78,7 +94,7 @@ Sarima = function(ts,order = c(1,0,0),seasonal = c(0,0,0),xreg = NULL,
   m1$prior_ma  = matrix(rep(c(0,0.5,1,1),m1$q),ncol = 4,byrow = TRUE)
   m1$n1 = m1$n
 
-  if(period == 0) m1$period = frequency(ts)
+  if(period == 0) m1$period = stats::frequency(ts)
   else m1$period = period
 
   m1$P = seasonal[1]
@@ -105,20 +121,21 @@ Sarima = function(ts,order = c(1,0,0),seasonal = c(0,0,0),xreg = NULL,
 
     # seasonal adjustment
     if(m1$D > 0) {
-      cat("seasonal difference is not allowed in dynamic regressions \n")
+      warning("seasonal difference is not allowed in dynamic regressions D  = 0 \n")
       m1$D = 0
     }
 
     m1$d1 = ncol(xreg)
     m1$xreg = xreg
+    m1$reg = xreg
     m1$prior_breg  = matrix(rep(c(0,2.5,6,4),m1$d1),ncol = 4,byrow = TRUE)
 
     if(m1$d > 0){
       m1$xreg = diff(m1$xreg,differences = m1$d)
       m1$xlast = matrix(,nrow = m1$d,ncol = m1$d1)
-      m1$xlast[1,] = tail(xreg,n=1)
+      m1$xlast[1,] = utils::tail(xreg,n=1)
       if(m1$d > 1)
-        for(i in 2:m1$d) m1$xlast[i,] = tail( diff(xreg,differences = i-1),n=1)
+        for(i in 2:m1$d) m1$xlast[i,] = utils::tail( diff(xreg,differences = i-1),n=1)
     }
   }
   else{
