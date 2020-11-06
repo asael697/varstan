b417b3ce4e6217a1ab2ebdba897c35a71317c3bd
asael-am <asael.alonzo@gmail.com>
2020-11-06 13:35:07 +0200

    Documentation update

--- R/auto_sarima.R
+++ R/auto_sarima.R
@@ -1,46 +1,46 @@
-#' Bayesian version of auto.arima  function
+#' Automatic estimate of a Seasonal ARIMA model
 #'
-#' Returns the best \code{Sarima} model using a \code{bic} value, this
-#' function is a bayesian adaptation of the auto.arima() function of the
-#' forecast package. Used as a starting point of your bayesian analysis.
-#'
+#' Returns the best seasonal ARIMA model using a \code{bic} value, this
+#' function the\code{auto.arima} function of the \pkg{forecast} package
+#' to select the seasonal ARIMA model and estimates the model using a
+#' HMC sampler.
 #'
 #' @usage auto.sarima(ts,xreg = NULL,chains=1,iter=4000,warmup=floor(iter/2),
 #'                 adapt.delta = 0.9,tree.depth =10,stepwise = TRUE,...)
 #'
-#' @param ts an univariate time series
-#' @param xreg	Optionally, a numerical matrix of external regressors,
+#' @param ts a numeric or ts object with the univariate time series.
+#' @param xreg Optionally, a numerical matrix of external regressors,
 #' which must have the same number of rows as ts. It should not be a data frame.
 #' @param chains An integer of the number of Markov Chains chains to be run,
-#' by default 1 chains is run
+#' by default 1 chain is run.
 #' @param iter An integer of total iterations per chain including the warm-up,
-#' by default  the number of iterations are 2000
+#' by default  the number of iterations are 2000.
 #' @param warmup  A positive integer specifying number of warm-up (aka burn-in)
-#'   iterations. This also specifies the number of iterations used for stepsize
-#'   adaptation, so warmup samples should not be used for inference. The number
+#'   iterations. This also specifies the number of iterations used for step-size
+#'   adaptation, so warm-up samples should not be used for inference. The number
 #'   of warmup should not be larger than \code{iter} and the default is
-#'   \code{iter/2}
+#'   \code{iter/2}.
 #' @param adapt.delta An optional real value between 0 and 1, the thin of the jumps
-#' in a HMC method. By default is 0.9
+#' in a HMC method. By default is 0.9.
 #' @param  tree.depth An integer of the maximum  depth of the trees  evaluated
-#' during each iteration. By default is 10
+#' during each iteration. By default is 10.
 #' @param stepwise	If TRUE, will do stepwise selection (faster). Otherwise, it searches
 #' over all models. Non-stepwise selection can be very slow, especially for seasonal models.
+#' @param ... Further arguments passed to  \code{auto.arima} function.
 #'
 #' @details
-#' Bayesian version of the automatic arima model fitting proposed by
-#' Robj Hyndman, this function finds the best Sarima model using \code{bic},
-#' and then proceeds to fit the model using \code{varstan} function and the default
-#' priors of a \code{Sarima} model constructor
+#' Automatic ARIMA model fitting implemented by Rob Hyndman, this function finds the best
+#' Seasonal ARIMA model using \code{bic}, and then proceeds to fit the model using
+#' \code{varstan} function and the default priors of a \code{Sarima} model constructor.
 #'
-#' This function provides an initial model fit for beginning the bayesian analysis
+#' This function provides an initial model fit for beginning the Bayesian analysis
 #' of the univariate time series. For better fit and model selection try different
-#' models and other moodel selection criteria such as \code{loo} or \code{bayes_factor}
+#' models and other model selection criteria such as \code{loo} or \code{bayes_factor}.
 #'
 #' The default arguments are designed for rapid estimation of models for many time series.
-#' If you are analysing just one time series, and can afford to take some more time,
-#' it is recommended that you set \code{stepwise}=\code{FALSE} and reduce the number of iterations
-#' per chain (\code{iter})
+#' If you are analyzing just one time series, and can afford to take some more time, it is
+#' recommended that you set \code{stepwise}=\code{FALSE} and reduce the number of iterations
+#' per chain (\code{iter}).
 #'
 #' For more information look at auto.arima function of forecast package.
 #'
@@ -54,18 +54,25 @@
 #' @seealso \code{Sarima} \code{varstan}.
 #'
 #' @references
-#'  Hyndman and Khandakar, Automatic Time Series Forecasting: The forecast
-#'  package for R.
+#' Hyndman, R. & Khandakar, Y. (2008). Automatic time series forecasting: the
+#' forecast package for \code{R}. \emph{Journal of Statistical Software}. 26(3),
+#' 1-22.\code{doi:	10.18637/jss.v027.i03}.
 #'
-#'  Hyndman Athanasopoulos, Forecasting: Principles and Practice
+#' Box, G. E. P. and Jenkins, G.M. (1978). Time series analysis: Forecasting and
+#' control. San Francisco: Holden-Day. \emph{Biometrika}, 60(2), 297-303.
+#' \code{doi:10.1093/biomet/65.2.297}.
+#'
+#' Kennedy, P. (1992). Forecasting with dynamic regression models: Alan Pankratz, 1991.
+#' \emph{International Journal of Forecasting}. 8(4), 647-648.
+#' \code{url: https://EconPapers.repec.org/RePEc:eee:intfor:v:8:y:1992:i:4:p:647-648}.
 #'
 #' @examples
 #' \dontrun{
-#' # Automatic Sarima model for the birth data
-#' auto.sarima(birth)
+#'  # Automatic Sarima model for the birth data
+#'  auto.sarima(birth)
 #'
-#' # Dyanimc Harmonic regression
-#' auto.sarima(birth,xreg = fourier(birth,K= 6))
+#'  # Dynamic Harmonic regression
+#'  auto.sarima(birth,xreg = fourier(birth,K= 6))
 #'}
 #'
 auto.sarima = function(ts,xreg= NULL,
@@ -91,12 +98,11 @@ auto.sarima = function(ts,xreg= NULL,
                 tree.depth = tree.depth)
   return(sf1)
 }
-#' Fourier terms for modelling seasonality
+#' Fourier terms for modelling seasonality.
 #'
 #' \code{fourier} returns a matrix containing terms from a Fourier series, up
 #' to order \code{K}, suitable for use in \code{\link{Sarima}} or
 #' \code{\link{auto.sarima}}.
-#'
 #'
 #' The period of the Fourier terms is determined from the time series
 #' characteristics of \code{x}. When \code{h} is missing, the length of
@@ -130,21 +136,12 @@ auto.sarima = function(ts,xreg= NULL,
 #' @keywords forecast
 #'
 #' @examples
-#'
-#' library(ggplot2)
 #' \dontrun{
-#' # Using Fourier series for a "ts" object
-#' # K is chosen to minimize the AICc
-#' deaths.model  <- auto.arima(USAccDeaths, xreg=fourier(USAccDeaths,K=5), seasonal=FALSE)
-#' deaths.fcast <- forecast(deaths.model, xreg=fourier(USAccDeaths, K=5, h=36))
-#' autoplot(deaths.fcast) + xlab("Year")
-#'
-#' # Using Fourier series for a "msts" object
-#' taylor.lm <- tslm(taylor ~ fourier(taylor, K = c(3, 3)))
-#' taylor.fcast <- forecast(taylor.lm,
-#'     data.frame(fourier(taylor, K = c(3, 3), h = 270)))
-#' autoplot(taylor.fcast)
+#'  library(astsa)
+#'  # Dynaimc Harmonic regression
+#'  auto.sarima(birth,xreg = fourier(birth,K= 6))
 #' }
+#'
 #' @importFrom forecast fourier
 #' @export
 #'
