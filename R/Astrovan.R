f83d5755e5a1e6108dc103783f9bf7dfb947aaff
asael-am <asael.alonzo@gmail.com>
2020-11-06 13:30:56 +0200

    Documentation upgrade

--- R/Astrovan.R
+++ R/Astrovan.R
@@ -1,9 +1,10 @@
-#' Random Multivariate time series Data
+#' Random Multivariate time series Data.
 #'
-#'
-#' A dataset containing randomly generated multivariate(d = 2) time series data that follows a var(1)-Bekk(1,1)-m(1) model
-#' as follows. the var process is a 0.1 matrix, the arch process has \code{alpha1} = 0.1(I)  and alpha1 = 0.3(I), the garch process
-#' has a beta1 = 0.2*I and the mgarch has a H = 0.15 matrix. Where I is the identity matrix.
+#' A dataset containing randomly generated multivariate(d = 2) time series data
+#' that follows a var(1)-Bekk(1,1)-m(1) model as follows. the VAR process is a 0.1 matrix,
+#' the ARCH process has \code{alpha1 = 0.1*I}  and \code{alpha1 = 0.3*I}, the GARCH process
+#' has a \code{beta1 = 0.2*I} and the M-GARCH has a \code{H = 0.15} matrix. Where I is the
+#' identity matrix.
 #'
 #' @docType data
 #'
@@ -13,4 +14,5 @@
 #'  \item{Y2}{Second time series}
 #' }
 #' @source Randomly generated data
+#'
 "Astrovan"
